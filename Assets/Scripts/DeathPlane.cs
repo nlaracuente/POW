0a04aad5bafcc6df167ab08683d21af01e8b7ac5
Nelson Laracuente <nlaracuente09@gmail.com>
2017-07-29 10:35:22 -0700

    added more walls and a deathplane which tiggers a respawn on objects that can respawn

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Detects when objects have fallen from the map
+/// </summary>
+public class DeathPlane : MonoBehaviour
+{
+    /// <summary>
+    /// Detects an object that enter it and respawns it
+    /// </summary>
+    /// <param name="other"></param>
+	void OnTriggerStay(Collider other)
+    {
+        IRespawnable respawnable = other.gameObject.GetComponent<IRespawnable>();
+        if(respawnable != null) {
+            respawnable.Respawn();
+        }
+    }
+}
