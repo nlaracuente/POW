1308791d24741e2285a1a63e11c0c46ede0aa260
Nelson Laracuente <nlaracuente09@gmail.com>
2017-07-30 22:53:38 -0700

    saving to try to merge prototype

---
+++ Assets/Scripts/MenuCanvas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// The Menu Canvas holds the menu options for the player
+/// It also contains the music for the game
+/// Handles "pausing/resuming" the game when the menu is enabled/disabled
+/// Quits the application when requested
+/// </summary>
+public class MenuCanvas : MonoBehaviour
+{
+
+    /// <summary>
+    /// Closes the menu
+    /// Resumes the game
+    /// </summary>
+    public void Play()
+    {
+
+    }
+
+    /// <summary>
+    /// Menu is triggered opened
+    /// </summary>
+    public void OpenMenu()
+    {
+
+    }
+
+    /// <summary>
+    /// When is closed
+    /// </summary>
+    public void CloseMenu()
+    {
+
+    }
+
+	// <summary>
+    /// Closes the app
+    /// </summary>
+    public void Exit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/TeleporterTile.cs
+++ Assets/Scripts/TeleporterTile.cs
@@ -7,4 +7,33 @@ using UnityEngine;
 /// </summary>
 public class TeleporterTile : Consumable
 {
+    /// <summary>
+    /// Material that represents the portal is active
+    /// </summary>
+    [SerializeField]
+    Material activeMaterial;
+
+    /// <summary>
+    /// Material that represents the portal is not active
+    /// </summary>
+    [SerializeField]
+    Material deactiveMaterial;
+
+    /// <summary>
+    /// A reference to the mesh renderer where we can change the material
+    /// </summary>
+    [SerializeField]
+    MeshRenderer meshRenderer;
+
+    /// <summary>
+    /// Updates the texture to match its current state
+    /// </summary>
+    void LateUpdate()
+    {
+        if(this.isActivated) {
+            this.meshRenderer.material = this.activeMaterial;
+        } else {
+            this.meshRenderer.material = this.deactiveMaterial;
+        }
+    }
 }
