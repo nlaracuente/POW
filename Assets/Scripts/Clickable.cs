42e169545bb9a0a0f0a5838d9c255f3687273c5e
Nelson Laracuente <nlaracuente09@gmail.com>
2017-08-01 17:33:30 -0700

    added the prefab, graphics, and scripts to allow mobile version

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// UIButtons notify the associated controller when a button
+/// has been pressed or released. Controllers are associated 
+/// based on the type of the button. The button itself is 
+/// passed to the controller to interprit the action
+/// </summary>
+public class Clickable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    /// <summary>
+    /// Delgates that this clickable object will dispatch
+    /// </summary>
+    /// <param name="value">Value of the object clicked</param>
+    public delegate void PressedDelegate(Clickable button);
+    public delegate void ReleasedDelegate(Clickable button);
+
+    /// <summary>
+    /// Registered listeners
+    /// </summary>
+    public event PressedDelegate PressedEvent;
+    public event ReleasedDelegate ReleasedEvent;
+
+    /// <summary>
+    /// The value of this clickable object
+    /// </summary>
+    public enum Value
+    {
+        // Directional Pad Inputs
+        DPad_Up,
+        DPad_Left,
+        DPad_Down,
+        DPad_Right,
+
+        // Warp to the last check point
+        Respawn,
+        Recall,
+        Action,
+    };
+
+    /// <summary>
+    /// Which clickable item this is
+    /// </summary>
+    [SerializeField]
+    Value value;
+    
+    /// <summary>
+    /// A reference to the image that represents this button
+    /// </summary>
+    Image buttonImage;
+
+    /// <summary>
+    /// Stores all references
+    /// </summary>
+    void Start()
+    {
+        this.buttonImage = GetComponent<Image>();
+    } // Start
+
+    /// <summary>
+    /// Returns True when the point of contact was made within the bounds
+    /// of this button
+    /// </summary>
+    /// <param name="ped">Information about the point of contact</param>
+    /// <returns>True: interaction occured on this button</returns>
+    bool InBounds(PointerEventData ped)
+    {
+        // Holds the position of the touch
+        Vector2 pos;
+
+        // Check if the touch is within the bounds on the background image
+        bool inBound = RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            this.buttonImage.rectTransform,
+            ped.position,
+            ped.pressEventCamera,
+            out pos
+        );
+
+        return inBound;
+    } // InBounds
+
+	/// <summary>
+    /// Called once when the touch input is detected
+    /// </summary>
+    /// <param name="ped">The input information</param>
+	public virtual void OnPointerDown(PointerEventData ped)
+    {
+        // Ignores interactions outside of this button
+        if( ! this.InBounds(ped) ) {
+            return;
+        }
+        
+        if(this.PressedEvent != null) {
+            this.PressedEvent(this);
+        }
+    } // OnPointerDown
+
+    /// <summary>
+    /// Called once when the touch input is no longer touching
+    /// </summary>
+    /// <param name="ped"></param>
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        // Ignores interactions outside of this button
+        if( ! this.InBounds(ped) ) {
+            return;
+        }
+
+        if(this.ReleasedEvent != null) {
+            this.ReleasedEvent(this);
+        }
+    } // OnPointerUp
+
+    /// <summary>
+    /// Returns the value of this clickable item
+    /// </summary>
+    public virtual Value GetValue()
+    {
+        return this.value;
+    }
+} // class
---
+++ Assets/Scripts/VirtualDPadController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles receiving and process player inputs
+/// </summary>
+public class VirtualDPadController : MonoBehaviour
+{
+    /// <summary>
+    /// Holds -1, 0, 1 to represent which button is being pressed
+    /// Where "Horizontal" is left[-1] / right[1]
+    /// And "Vertical" is down[-1] / up[1]
+    /// A value of 0 could mean both are pressed or neither is
+    /// </summary>
+    Dictionary<string, float> input = new Dictionary<string, float>{
+        { "Horizontal", 0f },
+        { "Vertical", 0f },
+        { "Magnet", 0f }
+    };
+
+    /// <summary>
+    /// Returns the current player directional input 
+    /// </summary>
+    public Dictionary<string, float> Input
+    {
+        get
+        {
+            return this.input;
+        }
+    }
+
+    /// <summary>
+    /// Translates the Clicable.Value button into an input value
+    /// </summary>
+    Dictionary<Clickable.Value, int> inputValue = new Dictionary<Clickable.Value, int> {
+        { Clickable.Value.DPad_Up,    1},
+        { Clickable.Value.DPad_Left, -1},
+        { Clickable.Value.DPad_Down, -1},
+        { Clickable.Value.DPad_Right, 1},
+        { Clickable.Value.Respawn,    1},
+        { Clickable.Value.Recall,     1},
+        { Clickable.Value.Action,     1},
+    };
+
+    /// <summary>
+    /// Register the controller to the child buttons to listed for the on pressed/released
+    /// </summary>
+    void Start()
+    {
+        foreach(Clickable clickable in GetComponentsInChildren<Clickable>()) {
+            clickable.PressedEvent += this.OnButtonPressed;
+            clickable.ReleasedEvent += this.OnButtonReleased;
+        }
+    }
+    
+    /// <summary>
+    /// Player pressed a virtual d-pad button
+    /// </summary>
+    /// <param name="value"></param>
+    void OnButtonPressed(Clickable button)
+    {
+        Clickable.Value value = button.GetValue();
+
+        // Default to Magnet
+        string inputType = "Magnet";
+
+        // Horizontal input
+        if(value == Clickable.Value.DPad_Left || value == Clickable.Value.DPad_Right) {
+            inputType = "Horizontal";
+        } 
+
+        // Vertical input
+        if(value == Clickable.Value.DPad_Down || value == Clickable.Value.DPad_Up) {
+            inputType = "Vertical";
+        }
+
+        this.input[inputType] += this.inputValue[value];
+    }
+
+    /// <summary>
+    /// Player released a virtual d-pad button
+    /// </summary>
+    /// <param name="value"></param>
+    void OnButtonReleased(Clickable button)
+    {
+        Clickable.Value value = button.GetValue();
+
+        // Default to Magnet
+        string inputType = "Magnet";
+
+        // Horizontal input
+        if(value == Clickable.Value.DPad_Left || value == Clickable.Value.DPad_Right) {
+            inputType = "Horizontal";
+        } 
+
+        // Vertical input
+        if(value == Clickable.Value.DPad_Down || value == Clickable.Value.DPad_Up) {
+            inputType = "Vertical";
+        }
+
+        // Only a single button was pressed before, it is safe to zero out
+        if(this.input[inputType] != 0) {
+            this.input[inputType] = 0;
+
+        // Both buttons were being pressed and now one has been released
+        // Therefore we use the inverse value of the button release to get the value of the one still pressed
+        } else {
+             this.input[inputType] = -this.inputValue[value];
+        }
+    }
+}
